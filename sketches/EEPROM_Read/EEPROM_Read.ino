673354ff8d400a7ec2b49e3d237d382014485470
electricalmuffin <33985200+electricalmuffin@users.noreply.github.com>
2017-12-14 20:30:59 +0500

    Добавил папку sketches и eeprom

---
+++ sketches/EEPROM_Read/EEPROM_Read.ino
@@ -0,0 +1,6 @@
+#include <EEPROM.h>
+
+//ВНИМАНИЕ! ВСАВЛЯТЬ В VOID SETUP()
+Serial.begin(9600);
+Serial.println(EEPROM.read(1));
+//EEPROM.read(1)
---
+++ sketches/EEPROM_Write/EEPROM_Write.ino
@@ -0,0 +1,10 @@
+#include <EEPROM.h>
+
+//ВНИМАНИЕ! ВСАВЛЯТЬ В VOID SETUP()
+Serial.begin(9600);
+EEPROM.write(1,животное); //См docs/saves.md
+Serial.println("1_Zapisano");
+EEPROM.write(2,время выдачи корма); //См docs/saves.md
+Serial.println("2_Zapisano");
+EEPROM.write(3,температура); //См docs/saves.md
+Serial.println("3_Zapisano");
